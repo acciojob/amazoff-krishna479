ac799de7f9914a4bfef027c7aa531a67cfc4f0b1
krishna479 <krishankant.sharma@gmail.com>
2023-02-22 17:25:58 +0530

    second

---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,137 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class OrderRepository {
+
+    HashMap<String,Order> orderMap= new HashMap<>();
+    HashMap<String,DeliveryPartner>partnerMap = new HashMap<>();
+    HashMap<String ,DeliveryPartner> OrderdeliveryMap= new HashMap<>();
+    HashMap<String,List<Order>> partnerOrdersMap = new HashMap<>();
+    public String addOrder(Order order) {
+orderMap.put((order.getId()),order);
+return "done";
+    }
+
+    public String addPartner(String partnerId) {
+        DeliveryPartner deliveryPartner =new DeliveryPartner(partnerId);
+
+        partnerMap.put(partnerId,deliveryPartner);
+        return "Done";
+    }
+
+    public String addOrderPartnerPair(String orderId, String partnerId) {
+          DeliveryPartner deliveryPartner=partnerMap.get(partnerId);
+         int order = deliveryPartner.getNumberOfOrders();
+         order =order+1;
+        deliveryPartner.setNumberOfOrders(order);
+         OrderdeliveryMap.put(orderId,deliveryPartner);
+
+         List<Order>list = new ArrayList<>();
+         list.add(orderMap.get(order));
+         partnerOrdersMap.put(partnerId,list);
+
+
+        return "successfully done";
+
+
+    }
+
+    public Order getOrderById(String orderId) {
+      return  orderMap.get(orderId);
+
+    }
+
+    public DeliveryPartner getPartnerById(String partnerId) {
+        return partnerMap.get(partnerId);
+    }
+
+
+    public Integer getOrderCountByPartnerId(String partnerId) {
+        int order=0;
+        for(DeliveryPartner deliveryPartner : OrderdeliveryMap.values()){
+            if(deliveryPartner.getId().equals(partnerId)){
+                order = deliveryPartner.getNumberOfOrders();
+            }
+        }
+        return order;
+    }
+
+    public List<Order> getOrdersByPartnerId(String partnerId) {
+
+  return  partnerOrdersMap.get(partnerId);
+
+    }
+
+    public List<Order> getAllOrders() {
+        List<Order>list = new ArrayList<>();
+        for(Order order:orderMap.values()){
+            list.add(order);
+        }
+        return list;
+    }
+
+    public Integer getCountOfUnassignedOrders() {
+        int countNoOrders =0;
+        int assignedOrder=0;
+        for(Order order:orderMap.values()){
+            countNoOrders++;
+        }
+        for(String s: OrderdeliveryMap.keySet()){
+            assignedOrder++;
+        }
+        return countNoOrders-assignedOrder;
+    }
+
+    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
+
+        String   stringHours = String.valueOf(time.charAt(0) + time.charAt(1));
+        int hours = Integer.parseInt(stringHours);
+        hours=hours*60;
+        String  stringMinutes = String.valueOf(time.charAt(3)+time.charAt(4));
+        int minutes= Integer.parseInt(stringMinutes);
+        int ans =hours+minutes;
+        int count=0;
+        for(List<Order>orders : partnerOrdersMap.values()){
+            for(Order order: orders){
+                if(order.getDeliveryTime()< ans){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public String deletePartnerById(String partnerId) {
+  //  DeliveryPartner partner=    partnerMap.get(partnerId);
+//    int id =
+//
+//        for(DeliveryPartner deliveryPartner : OrderdeliveryMap.values()){
+//            if(deliveryPartner.equals(partner){
+//
+//                OrderdeliveryMap.remove();
+//            }
+//        }
+        partnerMap.remove(partnerId);
+        partnerOrdersMap.remove(partnerId);
+        return "done";
+    }
+
+    public String deleteOrderById(String orderId) {
+        orderMap.remove(orderId);
+      DeliveryPartner partner=  OrderdeliveryMap.get(orderId);
+      partnerMap.remove(partner.getId());
+      OrderdeliveryMap.remove(orderId);
+      return "Success";
+
+    }
+
+    public String getLastDeliveryTimeByPartnerId(String partnerId) {
+        return "1";
+    }
+}
---
+++ src/main/java/com/driver/OrderService.java
@@ -0,0 +1,65 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService
+{
+@Autowired
+OrderRepository orderRepository;
+    public String addOrder(Order order) {
+     return orderRepository.addOrder(order);
+    }
+
+    public String addPartner(String partnerId) {
+        return orderRepository.addPartner(partnerId);
+    }
+
+    public String addOrderPartnerPair(String orderId, String partnerId) {
+       return  orderRepository.addOrderPartnerPair(orderId,partnerId);
+    }
+
+    public Order getOrderById(String orderId) {
+        return orderRepository.getOrderById(orderId);
+    }
+
+    public DeliveryPartner getPartnerById(String partnerId) {
+        return orderRepository.getPartnerById(partnerId);
+    }
+
+
+    public Integer getOrderCountByPartnerId(String partnerId) {
+        return orderRepository.getOrderCountByPartnerId(partnerId);
+    }
+
+    public List<Order> getOrdersByPartnerId(String partnerId) {
+        return orderRepository.getOrdersByPartnerId(partnerId);
+    }
+
+    public List<Order> getAllOrders() {
+        return  orderRepository.getAllOrders();
+    }
+
+    public Integer getCountOfUnassignedOrders() {
+        return orderRepository.getCountOfUnassignedOrders();
+    }
+
+    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
+      return  orderRepository.getOrdersLeftAfterGivenTimeByPartnerId(time,partnerId);
+    }
+
+    public String deletePartnerById(String partnerId) {
+        return orderRepository.deletePartnerById(partnerId);
+    }
+
+    public String deleteOrderById(String orderId) {
+        return  orderRepository.deleteOrderById(orderId);
+    }
+
+    public String getLastDeliveryTimeByPartnerId(String partnerId) {
+        return orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
+    }
+}
